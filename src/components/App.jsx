966a998bda0b080c05cfeca57b74108fed3e9276
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-10-06 20:42:10 +0300

    section-update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,22 @@ import 'react-toastify/dist/ReactToastify.css';
 import Section from './Section';
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/actions';
 
 export default function App() {
+  const contacts = useSelector(getContacts);
+
   return (
     <div>
       <Section title="Add contact">
         <ContactForm />
       </Section>
-      <Section title="Contacts">
-        <ContactList />
-      </Section>
+      {contacts.length > 0 && (
+        <Section title="Contacts">
+          <ContactList />
+        </Section>
+      )}
       <ToastContainer />
     </div>
   );
